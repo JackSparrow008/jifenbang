f8d2fad7056604fac21de596a87f9bf7351e3891
JackSparrow008 <chenqiong0623@qq.com>
2018-04-09 22:03:31 +0800

    jifenbang_2.0

---
+++ public/scores.js
@@ -0,0 +1,136 @@
+(function (window) {
+  var scores = [{
+      "id": "1",
+      "name": "陈碧辉",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "2",
+      "name": "陈保明",
+      "integral": "0",
+      "details": "查看明细"
+    },
+    {
+      "id": "3",
+      "name": "陈一梅",
+      "integral": "0",
+      "details": "查看明细"
+    },
+    {
+      "id": "4",
+      "name": "丁永强",
+      "integral": "0",
+      "details": "查看明细"
+    },
+    {
+      "id": "5",
+      "name": "戈壁川",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "6",
+      "name": "胡军",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "7",
+      "name": "刘荣苗",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "8",
+      "name": "李新",
+      "integral": "0",
+      "details": "查看明细"
+    },
+    {
+      "id": "9",
+      "name": "申屠宇静",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "10",
+      "name": "吴峰青",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "11",
+      "name": "吴子雨",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "12",
+      "name": "王旭",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "13",
+      "name": "徐建国",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "14",
+      "name": "徐洁莹",
+      "integral": "0",
+      "details": "查看明细"
+    },
+    {
+      "id": "15",
+      "name": "徐立松",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "16",
+      "name": "许天岚",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "17",
+      "name": "姚梁宇",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "18",
+      "name": "严萱",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "19",
+      "name": "俞亚峰",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "20",
+      "name": "郑国超",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "21",
+      "name": "朱剑",
+      "integral": "40",
+      "details": "查看明细"
+    },
+    {
+      "id": "22",
+      "name": "张俊华",
+      "integral": "40",
+      "details": "查看明细"
+    }
+  ];
+  window.scores = scores;
+})(this);
